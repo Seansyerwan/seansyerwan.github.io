978005888ce4816c742e6f504ded1825b4a4c59a
Seansyerwan <Seankerrigan122016@gmail.com>
2024-07-07 18:12:33 +0100

    added page2 music, updated javascript

--- website.js
+++ website.js
@@ -1,4 +1,16 @@
 var audio = new Audio("background_music.mp3");
+var page2_audio = new Audio("page2_music.mp3");
 
+audio.loop = true;
+page2_audio.loop = true;
+var currentpath = window.location.pathname;
 
-window.onload= audio.play();
+if(currentpath.includes("1")){
+    window.onload= audio.play();
+    console.log("Playing wii music");
+}
+else if (currentpath.includes("2")){
+    window.onload = page2_audio.play();
+    console.log("Playing clair de lune");
+}
+
