918400a17b9e80a23c5b4bd3e01074f9a814faac
Seansyerwan <Seankerrigan122016@gmail.com>
2024-07-07 18:46:28 +0100

    added toolbar calculations

--- website.js
+++ website.js
@@ -5,6 +5,8 @@ audio.loop = true;
 page2_audio.loop = true;
 var currentpath = window.location.pathname;
 
+
+//depending on the page, we play a song. 
 if(currentpath.includes("1")){
     window.onload= audio.play();
     console.log("Playing wii music");
@@ -14,3 +16,16 @@ else if (currentpath.includes("2")){
     console.log("Playing clair de lune");
 }
 
+//We calculate where the window has scrolled as to hide the menu bar. 
+var position = window.scrollY;
+window.onscroll = function() {
+    var currentpos = window.scrollY;
+
+    if(position > currentpos){
+        document.getElementById("general_toolbar").style.top = "0";
+    }
+    else{
+        document.getElementById("general_toolbar").style.top = "-50px";
+    }
+    position = currentpos;
+}
